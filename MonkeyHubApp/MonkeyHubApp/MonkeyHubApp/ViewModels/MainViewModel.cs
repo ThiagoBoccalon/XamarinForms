53d62ea6118ea02a1271c69a43ba31500f328441
Thiago Bucalon <thiago_rodrigo90@hotmail.com>
2017-09-26 16:19:18 -0300

    Aplicando mecanismo de Binding

--- MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -7,13 +7,34 @@ using System.Threading.Tasks;
 
 namespace MonkeyHubApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
-        public string Descricao { get; set; }
+        private string _descricao;
+
+        public string Descricao
+        {
+            get
+            {
+                return _descricao;
+            }
+            set
+            {
+                SetProperty(ref _descricao, value);
+            }
+        }
         
         public MainViewModel()
         {
             Descricao = "Olá mundo, feliz 2017!!!";
+
+            Task.Delay(2000).ContinueWith(async t =>
+            {
+                for (int i = 2018; i <= 2030; i++)
+                {
+                    await Task.Delay(1000);
+                    Descricao = $"Olá mundo, feliz {i}";
+                }
+            });
         }
     }
 }
