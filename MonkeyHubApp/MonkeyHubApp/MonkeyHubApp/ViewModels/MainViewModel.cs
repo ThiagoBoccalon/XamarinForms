a5f87be4730a1e6f40be2e652ed70d4f3ed3c77e
Thiago Bucalon <thiago_rodrigo90@hotmail.com>
2017-09-26 22:33:29 -0300

    Coleções com classes customizadas: uso de Json.

--- MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -2,12 +2,40 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using MonkeyHubApp.Models;
 
 namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
-    {        
-        private string _seachTerm;        
+    {      
+            private const string BaseUrl = "https://monkey-hub-api.azurewebsites.net/api/";
+
+            public async Task<List<Tag>> GetTagsAsync()
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await httpClient.GetAsync($"{BaseUrl}Tags").ConfigureAwait(false);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                    {
+                        return JsonConvert.DeserializeObject<List<Tag>>(
+                            await new StreamReader(responseStream)
+                                .ReadToEndAsync().ConfigureAwait(false));
+                    }
+                }
+
+                return null;
+            }
+
+
+       private string _seachTerm;        
 
         public string SearchTerm
         {
@@ -22,24 +50,18 @@ namespace MonkeyHubApp.ViewModels
             }
         }
 
-        public ObservableCollection<string> Resultados { get; }
+        public ObservableCollection<Tag> Resultados { get; }
 
         public Command SearchCommand
         {
             get;
-        }
-
-        public Command ClearCommand
-        {
-            get;
-        }
+        }      
 
 
         public MainViewModel()
         {
-            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
-            ClearCommand = new Command(ExecuteClearCommand, CanExecuteClearCommand);
-            Resultados = new ObservableCollection<string>();
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);            
+            Resultados = new ObservableCollection<Tag>();
         }
        
         async void ExecuteSearchCommand()
@@ -48,28 +70,30 @@ namespace MonkeyHubApp.ViewModels
 
             // await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você pesquisou por '{SearchTerm}'.", "OK");
             bool resposta = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você pesquisou po '{SearchTerm}'?", "Sim", "Não");
+
             if (resposta)
+            {
                 await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "Obrigado", "Ok");
+                
+                var tagsRetornadasDoServico = await GetTagsAsync();
+                Resultados.Clear();
+
+                if (tagsRetornadasDoServico != null)
+                {
+                    foreach (var tag in tagsRetornadasDoServico)
+                        Resultados.Add(tag);
+                }
+            }
             else
             {
                 await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "Pesquise Novamente!", "OK");
-                Resultados.Add(SearchTerm);         
+                Resultados.Clear();
             }
         }
 
         bool CanExecuteSearchCommand()
         {          
             return string.IsNullOrWhiteSpace(SearchTerm) == false;
-        }
-
-        void ExecuteClearCommand()
-        {           
-           Resultados.Clear();
-        }
-
-        bool CanExecuteClearCommand()
-        {
-            return true; 
-        }
+        }       
     }
 }
